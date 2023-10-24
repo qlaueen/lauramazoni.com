6f964a319bd8fa050a4f610b7a5bcbb7479da7ff
qlaueen <qlaueen@gmail.com>
2023-10-24 12:44:04 -0400

    added styling to login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,45 @@
 <x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo"></x-slot>
+    <x-slot name="logo"></x-slot>
 
-        <!-- Session Status -->
-        <x-auth-session-status :status="session('status')" />
+    <div class="container justify-content-center d-flex mt-5">
+        <div class="card" style="width: 25rem;">
+            <!-- Session Status -->
+            <x-auth-session-status :status="session('status')" />
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors :errors="$errors" />
+            <!-- Validation Errors -->
+            <x-auth-validation-errors :errors="$errors" />
+            <div class="card-body">
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+                    <div class="card-text">
+                        
+                        <!-- Email Address -->
+                        <x-label class="form-label" for="email" :value="__('Email')" />
+                        <x-input class="form-control"  id="email" type="email" name="email" :value="old('email')" required autofocus />
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
+                        <!-- Password -->
+                        <x-label class="form-label mt-3" for="password" :value="__('Password')" />
+                        <x-input class="form-control"  id="password" type="password" name="password" required autocomplete="current-password" />
+                    
+                        <!-- Remember Me -->
+                        <label class="form-label mt-3" for="remember_me">
+                            <input class="form-check-input" id="remember_me" type="checkbox">
+                            <span>{{ __('Remember me') }}</span>
+                        </label>
+                    
 
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
-                <x-input id="email" type="email" name="email" :value="old('email')" required autofocus />
+                        <div class="d-flex justify-content-between align-items-center">
+                            @if (Route::has('password.request'))
+                                <a class="align-items-center" href="{{ route('password.request') }}">
+                                    {{ __('Forgot your password?') }}
+                                </a>
+                            @endif
+
+                            <button class="btn btn-primary">{{ __('Log in') }}</button>
+                        </div>
+                    </div>
+                </form>
             </div>
-
-            <!-- Password -->
-            <div>
-                <x-label for="password" :value="__('Password')" />
-                <x-input id="password" type="password" name="password" required autocomplete="current-password" />
-            </div>
-
-            <!-- Remember Me -->
-            <div>
-                <label for="remember_me">
-                    <input id="remember_me" type="checkbox">
-                    <span>{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div>
-                @if (Route::has('password.request'))
-                    <a href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-button>
-                    {{ __('Log in') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
+        </div>
+    </div>
 </x-guest-layout>
